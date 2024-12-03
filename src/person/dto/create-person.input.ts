a8ed6b7cc59bc96c0b020ebe1c1cca707c5af442
Jason Suarez <suarezjason@icloud.com>
2024-12-03 09:27:15 +0100

    refactor(validation): improve person input validation
    - Add clearer validation messages
    - Simplify phone number validation to 10 digits
    - Add minimum length validation for names

--- src/person/dto/create-person.input.ts
+++ src/person/dto/create-person.input.ts
@@ -1,22 +1,25 @@
 // src/person/dto/create-person.input.ts
 import { InputType, Field } from '@nestjs/graphql';
-import { IsString, IsEmail, IsPhoneNumber } from 'class-validator';
+import { IsString, IsEmail, Matches, MinLength } from 'class-validator';
 
 @InputType()
 export class CreatePersonInput {
   @Field()
   @IsString()
+  @MinLength(1, { message: 'Last name cannot be empty' })
   lastName: string;
 
   @Field()
   @IsString()
+  @MinLength(1, { message: 'First name cannot be empty' })
   firstName: string;
 
   @Field()
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
   @Field()
-  @IsPhoneNumber()
+  @IsString()
+  @Matches(/^\d{10}$/, { message: 'Phone number must be 10 digits' })
   phoneNumber: string;
 }
